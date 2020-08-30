0b5b2d404140f09b82a90a993fb53b5897ac244b
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-08-30 16:36:56 -0300

    Add state
    Now you can mark a todo as completed and delete them.

--- src/Todo.js
+++ src/Todo.js
@@ -2,11 +2,32 @@ import React from "react";
 import "./Todo.css";
 
 export default class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // initial state of each Todo
+    this.state = {
+      completed: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // event handler to mark a Todo as completed or not
+  handleClick() {
+    this.setState({
+      completed: !this.state.completed,
+    });
+  }
+
   render() {
+    const buttonText = this.state.completed ? "not completed" : "completed";
+    const completedStyle = this.state.completed ? "completed" : "";
     return (
-      <div className="todo">
+      <div className={`todo ${completedStyle}`}>
         <h3>{this.props.title}</h3>
         <p>{this.props.description}</p>
+        <button onClick={this.handleClick}>Mark as {buttonText}</button>
+        <button onClick={this.props.handleDelete}>Delete</button>
       </div>
     );
   }
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,32 +2,53 @@ import React from "react";
 import Todo from "./Todo.js";
 
 export default class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // initial todos
+    this.state = {
+      todos: [
+        {
+          id: 0,
+          title: "Build a vanilla JS App",
+          description: "Gotta practice the basics.",
+        },
+        {
+          id: 1,
+          title: "Build a React App",
+          description: "Practice makes perfect!.",
+        },
+        {
+          id: 2,
+          title: "Study more",
+          description: "I have to improve!.",
+        },
+      ],
+    };
+  }
+
+  // event handler, this one is passed as a prop to each Todo component, this way, each child (Todo) has a way to tell
+  // their parent (TodoList) to re-render them with new props.
+  handleDelete(id) {
+    const newTodos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: newTodos,
+    });
+  }
+
   render() {
-    const todos = this.props.todos.map((todo) => {
+    const todos = this.state.todos.map((todo) => {
       return (
-        <Todo key={todo.id} title={todo.title} description={todo.description} />
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          description={todo.description}
+          handleDelete={this.handleDelete.bind(this, todo.id)} // method binding and also pass the id of each todo to be able to tell which one to delete
+        />
       );
     });
     return <main>{todos}</main>;
   }
 }
-
-TodoList.defaultProps = {
-  todos: [
-    {
-      id: 0,
-      title: "Build a vanilla JS App",
-      description: "Gotta practice the basics.",
-    },
-    {
-      id: 1,
-      title: "Build a React App",
-      description: "Practice makes perfect!.",
-    },
-    {
-      id: 2,
-      title: "Study more",
-      description: "I have to improve!.",
-    },
-  ],
-};
