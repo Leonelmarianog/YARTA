7b9b399b7c6968823cb3f2bc48e0ca65acdfa4f7
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-08-30 16:02:40 -0300

    Updates

---
+++ precache-manifest.553c7921b37b87ed870f60edcff65c86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2300bab762457b319bf543bdbb8e1703",
+    "url": "/YARTA/index.html"
+  },
+  {
+    "revision": "c7297630523a116055b2",
+    "url": "/YARTA/static/css/main.a0d490bb.chunk.css"
+  },
+  {
+    "revision": "b9242f91d5701e8301ae",
+    "url": "/YARTA/static/js/2.1ad3a84e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YARTA/static/js/2.1ad3a84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7297630523a116055b2",
+    "url": "/YARTA/static/js/main.c6175732.chunk.js"
+  },
+  {
+    "revision": "71e16867aa5cbc6a7d7d",
+    "url": "/YARTA/static/js/runtime-main.24a30257.js"
+  }
+]);
