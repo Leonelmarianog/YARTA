6dcfb45a6e2b9fddecd9bedfd14b1235fc9e97d0
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-09-01 15:01:37 -0300

    Change component from class to functional
    Since the Todo component doesn't have its own state anymore, I don't
    need it to be a class component.

--- src/Todo.js
+++ src/Todo.js
@@ -2,28 +2,24 @@ import React from "react";
 import TodoForm from "./TodoForm.js";
 import "./Todo.css";
 
-export default class Todo extends React.Component {
-  render() {
-    const buttonText = this.props.isComplete ? "not completed" : "completed";
-    const completedStyle = this.props.isComplete ? "completed" : "";
-    const editForm = this.props.isFormVisible ? (
-      <TodoForm
-        title={this.props.title}
-        description={this.props.description}
-        handleEdit={this.props.handleEdit}
-      />
-    ) : null;
-    return (
-      <div className={`todo ${completedStyle}`}>
-        <h3>{this.props.title}</h3>
-        <p>{this.props.description}</p>
-        <button onClick={this.props.handleComplete}>
-          Mark as {buttonText}
-        </button>
-        <button onClick={this.props.handleForm}>Edit</button>
-        <button onClick={this.props.handleDelete}>Delete</button>
-        {editForm}
-      </div>
-    );
-  }
+export default function Todo(props) {
+  const buttonText = props.isComplete ? "not completed" : "completed";
+  const completedStyle = props.isComplete ? "completed" : "";
+  const editForm = props.isFormVisible ? (
+    <TodoForm
+      title={props.title}
+      description={props.description}
+      handleEdit={props.handleEdit}
+    />
+  ) : null;
+  return (
+    <div className={`todo ${completedStyle}`}>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+      <button onClick={props.handleComplete}>Mark as {buttonText}</button>
+      <button onClick={props.handleForm}>Edit</button>
+      <button onClick={props.handleDelete}>Delete</button>
+      {editForm}
+    </div>
+  );
 }
