3be88725e87ce9a5aecd5bc9bb7f42552e1dab1d
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-09-02 19:30:10 -0300

    Remove title margin

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
       <nav className="level">
         <div className="level-item py-5">
           <div>
-            <h1 className="title is-spaced is-size-2 is-size-4-mobile has-text-white">
+            <h1 className="title is-spaced is-size-2 is-size-4-mobile has-text-white mt-0">
               YARTA: Yet Another React Todo List
             </h1>
             <h2 className="subtitle is-size-5 has-text-white has-text-centered">
