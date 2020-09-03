b45b9b427f8fc044ad639dff649599fb990c95a9
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-09-03 18:39:21 -0300

    Updates Header
    Fixed title error, modified subtitle and made both more responsive.

--- src/Header.js
+++ src/Header.js
@@ -6,11 +6,18 @@ export default function Header() {
       <nav className="level">
         <div className="level-item py-5">
           <div>
-            <h1 className="title is-spaced is-size-2 is-size-4-mobile has-text-white mt-0">
-              YARTA: Yet Another React Todo List
+            <h1 className="title is-spaced is-size-2 is-size-5-mobile is-size-4-tablet is-size-3-desktop has-text-white mt-0">
+              YARTA: Yet Another React Todo App
             </h1>
-            <h2 className="subtitle is-size-5 has-text-white has-text-centered">
-              Made by Leonel Gauna - 2020
+            <h2 className="subtitle is-size-5 is-size-6-mobile is-size-5-tablet is-size-4-desktop has-text-white has-text-centered">
+              Made with{" "}
+              <span
+                style={{ color: "crimson" }}
+                className="is-size-4 is-size-5-mobile is-size-4-tablet is-size-3-desktop"
+              >
+                ♥
+              </span>{" "}
+              by Leonel Gauna
             </h2>
           </div>
         </div>
